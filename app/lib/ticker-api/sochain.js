2ac7518bf7b7a165508cb685bad836eaadf3683f
frndxyz <pravin_bhudiya@yahoo.com>
2016-02-24 13:58:23 +0530

    Update sochain.js

--- app/lib/ticker-api/sochain.js
+++ app/lib/ticker-api/sochain.js
@@ -7,7 +7,7 @@ var apiAURRoot = "https://bittrex.com/api/v1.1/public/getticker?market=BTC-AUR"
 
 function ltcToBtc(callback){
  
-  var corsUri = process.env.PROXY_URL + "?url=" + encodeURIComponent(apiLTCRoot)
+  var corsUri = process.env.PROXY_URL  + encodeURIComponent(apiLTCRoot)
   
   xhr({
     uri: corsUri ,
@@ -24,7 +24,7 @@ function ltcToBtc(callback){
 }
 
 function aurToBtc(callback){
-   var corsUri = process.env.PROXY_URL + "?url=" + encodeURIComponent(apiAURRoot)
+   var corsUri = process.env.PROXY_URL  + encodeURIComponent(apiAURRoot)
   
   xhr({
     uri:corsUri,
