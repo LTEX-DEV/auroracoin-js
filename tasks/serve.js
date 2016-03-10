e19befd9247d21fb48a0c08b2c40a6433f12b0b5
frndxyz <pravin_bhudiya@yahoo.com>
2016-03-10 15:23:08 +0530

    Update serve.js

--- tasks/serve.js
+++ tasks/serve.js
@@ -1,11 +1,22 @@
 var buildServer = require('../server/express')
 var done = require('./util').done
+var https = require('https')
+
+var pkey = fs.readFileSync('key.pem');
+var pcert = fs.readFileSync('cert.pem')
+
+var options = {
+    key: pkey,
+    cert: pcert
+};
 
 
 function serve(callback) {
-  var serverport = 8080
-  var server = buildServer()
-  server.listen(serverport)
+ 
+ var serverport = (process.env.NODE_ENV === 'production')? 443:80
+ 
+ https.createServer(options, buildServer()).listen(serverport);
+ 
 
   done('server', 'start', callback)()
 }
