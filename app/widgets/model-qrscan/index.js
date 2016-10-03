862e83fd8fd2ac5bca6e74a3ec0f4311da1b4d23
frndxyz <pravin_bhudiya@yahoo.com>
2016-10-03 15:15:59 +0530

    Update index.js

--- app/widgets/model-qrscan/index.js
+++ app/widgets/model-qrscan/index.js
@@ -2,7 +2,6 @@
 
 var Ractive = require('hive-modal')
 var emitter = require('hive-emitter')
-var qrcode = require('hive-qrcode')
 var getNetwork = require('hive-network')
 
 module.exports = function showTooltip(){
@@ -18,10 +17,45 @@ module.exports = function showTooltip(){
     amount:""
     }    
   })
-
-  var canvas = ractive.nodes['qrscan-canvas']
-  var qr = qrcode(getNetwork() + ':' + ractive.get('address'))
-  canvas.appendChild(qr)
+  
+  var qrDecoder=new QCodeDecoder()
+  
+ if(!qr.isCanvasSupported()||!qr.hasGetUserMedia())
+ {
+ var msgNode=ractive.nodes['msg']
+ 
+ msgNode.appendChild('<span>Your browser does not match the required specs. Canvas and getUserMedia are require. </span>')
+ 
+ }
+ractive.on('capture',function(){
+   var canvas = ractive.nodes['camera']
+   
+   qrDecoder.decodeFromCamera(canvas, function(er,res){
+     
+    var msgNode=ractive.nodes['msg']
+     
+     if(er)
+     {
+     msgNode.appendChild('<span>' + er + '</span>')
+     }else
+     {
+     msgNode.appendChild('<span>' + res + '</span>')
+     
+     var splt=res.split("?")
+     
+     var address=splt[0].split(":")[1]
+     var amount=splt[1].split("=")[1]
+     
+     ractive.set('address',address)
+     ractive.set('amount',amount)
+     
+     }
+      
+      
+  })
+ 
+})
+ 
 
   ractive.on('close', function(){
     ractive.fire('cancel')
