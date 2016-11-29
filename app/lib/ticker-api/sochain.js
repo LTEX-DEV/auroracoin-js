3adb70a158d2e5b563fd5ea274a372ed96880447
frndxyz <pravin_bhudiya@yahoo.com>
2016-11-29 15:57:16 +0530

    changes for auroracoin

--- app/lib/ticker-api/sochain.js
+++ app/lib/ticker-api/sochain.js
@@ -1,11 +1,32 @@
 'use strict';
 
 var xhr = require('xhr')
-var apiRoot = "https://chain.so/api/v2/get_price/LTC/BTC"
 
-function ltcToBtc(callback){
+//var apiLTCRoot = "https://bittrex.com/api/v1.1/public/getticker?market=BTC-LTC"
+var apiAURRoot = "https://bittrex.com/api/v1.1/public/getticker?market=BTC-AUR"
+
+/*function ltcToBtc(callback){
+ 
+  var corsUri = process.env.PROXY_URL  + encodeURIComponent(apiLTCRoot)
+  
   xhr({
-    uri: apiRoot,
+    uri: corsUri ,
+    timeout: 10000,
+    method: 'GET'
+  }, function(err, resp, body){
+    if(resp.statusCode !== 200) {
+      console.error(body)
+      return callback(err)
+    }
+    callback(null, toRate(JSON.parse(body)))
+  })
+}*/
+
+function aurToBtc(callback){
+   var corsUri = process.env.PROXY_URL  + encodeURIComponent(apiAURRoot)
+  
+  xhr({
+    uri:corsUri,
     timeout: 10000,
     method: 'GET'
   }, function(err, resp, body){
@@ -18,7 +39,17 @@ function ltcToBtc(callback){
   })
 }
 
+
+
+
 function toRate(res){
+  var price=res.result.Last
+  return price
+}
+
+
+
+/*function toRate(res){
   var validQuotes = res.data.prices
   .map(function(quote){
     return parseFloat(quote.price)
@@ -26,17 +57,17 @@ function toRate(res){
   .filter(function(price){
     return price === price && price !== 0 // not NaN nor 0
   })
-
   return validQuotes.reduce(function(memo, price, i, array){
     var total = memo + price
     if(i === array.length - 1){
       return total / array.length
     }
-
     return total
   }, 0)
 }
+*/
 
 module.exports = {
-  ltcToBtc: ltcToBtc
+ // ltcToBtc: ltcToBtc,
+  aurToBtc: aurToBtc
 }
